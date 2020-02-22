f52ebbf671059b10a154512ae93d0ef861980203
NatyNatur <naticcg@gmail.com>
2020-02-22 14:39:49 -0300

    adding input

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardComponent implements OnInit {
+export class CardComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  @Input() inCard: any;
+  dataCard: any;
+
+  constructor() {
+    console.log('card constructor')
+   }
 
   ngOnInit(): void {
+    console.log('card iniciado')
   }
+
+  ngOnChanges(): void {
+    console.log('card ha cambiado');
+    this.dataCard = this.inCard;
+  }
+
 
 }
