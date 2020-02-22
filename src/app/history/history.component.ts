5b7a49587920327ba537217b13e6e0156440a690
NatyNatur <naticcg@gmail.com>
2020-02-22 14:59:08 -0300

    adding history

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'history',
   templateUrl: './history.component.html',
   styleUrls: ['./history.component.scss']
 })
-export class HistoryComponent implements OnInit {
+export class HistoryComponent implements OnInit, OnChanges {
+
+  @Input() inHistory: any;
+  dataHistory:any = [];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngOnChanges(): void {
+    if(this.inHistory) {
+      this.dataHistory.push(this.inHistory)
+    }
+  }
 }
