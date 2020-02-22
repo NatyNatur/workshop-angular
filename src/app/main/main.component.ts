e7e716b912ae6cca7f3085cd6428361b3e60dffe
NatyNatur <naticcg@gmail.com>
2020-02-22 12:40:01 -0300

    adding components

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'main',
+    templateUrl: './main.component.html',
+    styleUrls: ['./main.component.scss']
+
+})
+
+export class MainComponent{
+    // se inyectan los servicios a utilizar dentro del componente
+    constructor() {
+
+    }
+
+}
